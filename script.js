82809d8dcfee4782ebd2e0d429c2411fe94b54a2
PoojaRamaraj <119429335+Poojaram18@users.noreply.github.com>
2023-01-26 12:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+
+let inputBox= document.createElement("input");
+inputBox.setAttribute("type","text");
+inputBox.setAttribute("id","evaluatedText");
+document.body.append(inputBox);
+let h3Ele=document.createElement("h3");
+h3Ele.setAttribute("id","wordCount");
+h3Ele.innerText='0';
+document.body.append(h3Ele);
+let arrOfWords;
+
+document.getElementById("evaluatedText").addEventListener("keyup",()=>{
+     let wordArr=inputBox.value.trim().split(/\s+/);
+     let word_count=wordArr.length;
+     if(word_count==1 && wordArr[0]=="") word_count=0;
+        document.getElementById("wordCount").innerText=word_count;
+    })
